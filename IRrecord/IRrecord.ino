7132c638c36440e4dcf4fc29a0843b2846f636b1
JD Russo <thejesusrambo@gmail.com>
2014-01-07 14:28:49 -0800

    Stylistic changes, better formatting

--- IRrecord/IRrecord.ino
+++ IRrecord/IRrecord.ino
@@ -26,8 +26,8 @@
 #include <SPI.h>
 
 #if defined(__SAM3X8E__)
-    #undef __FlashStringHelper::F(string_literal)
-    #define F(string_literal) string_literal
+#undef __FlashStringHelper::F(string_literal)
+#define F(string_literal) string_literal
 #endif
 
 Adafruit_ST7735 tft = Adafruit_ST7735(cs, dc, rst);
@@ -82,33 +82,33 @@ struct signal EEPSearch(char* query)
   Serial.write("\nBeginning EEPROM search for ");
   Serial.println(query);
   int startingByte = 0;
-  
+
   struct signal resultCommand;
   struct signal tempRead;
   memset(&resultCommand, 0, 16);
   //char temp[8];
-  
+
   for(int i = 0; i < 64;i++)
   {
     startingByte = i*16;
     EEPROM.readBlock(startingByte,tempRead);
-    
+
     memcpy(&resultCommand, &tempRead, 16);
-    
+
     Serial.print("Found something at memory address ");
     Serial.print(startingByte);
     Serial.print(" called ");
     Serial.print(resultCommand.name);
     Serial.print(" with code ");
     Serial.println(resultCommand.code,HEX);
-    
+
     if (strcmp(resultCommand.name,query) == 0)
     {
       Serial.println("Match found.");
       break;
     }
   }
-  
+
   //Son, it's a glorious time in an Arduino's life when he's searched through EEPROM to find
   //a data structure and found the one he's looking for. Let's return that shit and dip.
   return resultCommand;
@@ -170,31 +170,31 @@ void storeCode(decode_results *results)
         {
           for(int i=0;i<sizeof(commands)/sizeof(*commands);i++)
           { 
-            
+
             Serial.println("Iterating...");
             Serial.write("Name: ");
             Serial.println(commands[i].code);
-  
+
             //If commands[i] is empty, then write to it
             if (commands[i].code == 0)
             {
               memcpy(&commands[i].name,&writename,sizeof(commands[i].name));
               memcpy(&commands[i].code,&results->value,sizeof(commands[i].code));
               memcpy(&commands[i].len,&results->bits,sizeof(commands[i].len));
-              
+
               Serial.write("\nWrote: ");
               Serial.println(writename);
               Serial.write("\nVerify: ");
               Serial.println(commands[i].name);
               Serial.println(commands[i].code);
               Serial.println(commands[i].len);
-              
+
               break;
             }
           } // end FOR
         }
         pendingWrite = false;
-        
+
         if(EEPROMwrite)
         {
           struct signal tempCommand;
@@ -205,11 +205,11 @@ void storeCode(decode_results *results)
           for(int i = 0;i<64;i++)
           {
             int startingByte = 16*i;
-            
+
             //Check for a 0 starting byte
             if (EEPROM.read(startingByte) == 0)
             {
-              
+
               EEPROM.updateBlock(startingByte,tempCommand);
               Serial.print("Entered ");
               Serial.print(tempCommand.name);
@@ -220,8 +220,8 @@ void storeCode(decode_results *results)
               break;
             }
           }
-        EEPROMwrite = false;
-        pendingWrite = false;
+          EEPROMwrite = false;
+          pendingWrite = false;
         }
       }
     } 
@@ -305,44 +305,44 @@ void sendCode(int repeat)
 struct signal setColor(char* searchparam)
 {
   uint16_t textColor = ST7735_WHITE;
-  
+
   tft.fillRect(0,72,128,87,ST7735_BLACK);
-  
+
   Serial.print(F("\nSearching for "));
   Serial.println(searchparam);
-  
+
   struct signal colorcommand = EEPSearch(searchparam);
-  
+
   Serial.println(F("Attempting to send."));
   irsend.sendNEC(colorcommand.code, colorcommand.len);
-  
+
   Serial.print(F("First char: "));
   Serial.println(colorcommand.name[0]);
-  
+
   switch(colorcommand.name[0]){
-    case 'g':
-      textColor = ST7735_GREEN;
-      break;
-    case 'r':
-      textColor = ST7735_RED;
-      break;
-    case 'b':
-      textColor = ST7735_BLUE;
-      break;
-    case 'y':
-      textColor = ST7735_YELLOW;
-      break;
-    default:
-      textColor = ST7735_BLUE;
-      break;
+  case 'g':
+    textColor = ST7735_GREEN;
+    break;
+  case 'r':
+    textColor = ST7735_RED;
+    break;
+  case 'b':
+    textColor = ST7735_BLUE;
+    break;
+  case 'y':
+    textColor = ST7735_YELLOW;
+    break;
+  default:
+    textColor = ST7735_BLUE;
+    break;
   }
-  
+
   Serial.print(F("First char: "));
   Serial.println(colorcommand.name[0]);
-  
+
   drawText("Settinglights to ", ST7735_WHITE, 3);
   drawText(0,72,colorcommand.name,textColor, 3);
-  
+
   Serial.print(F("Sent: "));
   Serial.println(colorcommand.name);
   Serial.println(colorcommand.code, HEX);
@@ -384,14 +384,14 @@ int lastButtonState;
 String tempcontent;
 
 void loop() {
-  
+
   tempcontent = "";
-  Serial.println("Loop begun.");
-  
+ // Serial.println("Loop begun.");
+
   memset(&content,'\0',sizeof(content));
-  
-  Serial.println("Strings cleared.");
-  
+
+// Serial.println("Strings cleared.");
+
   while(Serial.available()) 
   {
     delay(10);
@@ -402,7 +402,7 @@ void loop() {
   Serial.write(content.c_str());
 
 
-Serial.println("String checks");
+ // Serial.println("String checks");
 
   //Check for read command (Read from memory, display code)
   if(strcmp(content.substring(0,4).c_str(),"read") == 0)
@@ -412,7 +412,7 @@ Serial.println("String checks");
     Serial.write("\nReading: ");
     Serial.println(readname);
   }
-  
+
   //Check for write command (Write detected code to memory)
   else if(strcmp(content.substring(0,5).c_str(),"write") == 0)
   {
@@ -421,7 +421,7 @@ Serial.println("String checks");
     Serial.write("\nWriting: ");
     Serial.write(writename);
   }
-  
+
   //Write detected code to EEPROM
   else if(strcmp(content.substring(0,8).c_str(),"eepwrite") == 0)
   {
@@ -431,25 +431,25 @@ Serial.println("String checks");
     Serial.write("\nWriting to EEPROM: ");
     Serial.write(writename);
   }
-  
+
   else if(strcmp(content.substring(0,7).c_str(),"eepread") == 0)
   {
     String searchstring = content.substring(8);
     char* searchparam = strdup(searchstring.c_str());
-    
+
     Serial.println("\nEEPREAD Received...");
     Serial.println(searchparam);
     setColor(searchparam);
   }
-  
+
   else if(strcmp(content.substring(0,7).c_str(),"eeprevw") == 0)
   {
     String searchstring = content.substring(8);
     char* searchparam = strdup(searchstring.c_str());
-    
+
     Serial.println(F("\nEEPREVW Received..."));
     struct signal command;
-    
+
     char temparray[16];
     temparray[0] = EEPROM.read(0);
     temparray[1] = EEPROM.read(1);
@@ -467,15 +467,15 @@ Serial.println("String checks");
     temparray[13] = EEPROM.read(13);
     temparray[14] = EEPROM.read(14);
     temparray[15] = EEPROM.read(15);
-    
+
     memcpy(&command,&temparray,16);
-    
+
     Serial.print(F("Attempting to send: "));
     Serial.println(command.name);
     Serial.println(command.code, HEX);
     command = setColor(command.name);
   }//As soon as it leaves this loop, it crashes the program ???
-  
+
   if(strcmp(content.substring(0,4).c_str(),"send") == 0)
   {
     char* searchparam = strdup(content.substring(5).c_str());
@@ -483,7 +483,7 @@ Serial.println("String checks");
   }
 
 
-Serial.println("Read check");
+//  Serial.println("Read check");
 
   if (pendingRead)
   {
@@ -507,13 +507,13 @@ Serial.println("Read check");
 
   // If button pressed, send the code.
   int buttonState = digitalRead(BUTTON_PIN);
-//  if (lastButtonState == LOW && buttonState == HIGH) 
-//  {
-//    Serial.println("Released");
-//    //irrecv.enableIRIn(); // Re-enable receiver
-//  }
+  //  if (lastButtonState == LOW && buttonState == HIGH) 
+  //  {
+  //    Serial.println("Released");
+  //    //irrecv.enableIRIn(); // Re-enable receiver
+  //  }
 
-Serial.println("Button state check");
+//  Serial.println("Button state check");
 
   if (!buttonState) 
   {
@@ -532,23 +532,24 @@ Serial.println("Button state check");
     digitalWrite(STATUS_PIN, LOW);
     delay(50); // Wait a bit between retransmissions
   }
-  
-  
-Serial.print("Decode check: ");
-Serial.println(pendingWrite);
 
-//  if(pendingWrite == true){
-//    if (irrecv.decode(&results)) 
-//    {
-//      Serial.println("Decoding...");
-//      digitalWrite(STATUS_PIN, HIGH);
-//      storeCode(&results);
-//      irrecv.resume(); // resume receiver
-//      digitalWrite(STATUS_PIN, LOW);
-//    }
-//  }
+
+//  Serial.print("Decode check: ");
+//  Serial.println(pendingWrite);
+
+    if(pendingWrite == true){
+      if (irrecv.decode(&results)) 
+      {
+        Serial.println("Decoding...");
+        digitalWrite(STATUS_PIN, HIGH);
+        storeCode(&results);
+        irrecv.resume(); // resume receiver
+        digitalWrite(STATUS_PIN, LOW);
+      }
+    }
   lastButtonState = buttonState;
-  
-//Serial.println("Loop finished.");
+
+  //Serial.println("Loop finished.");
 }
+
 
